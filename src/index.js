656eadd80a5d9bac50d65a33618c97e851f004d8
Anfey <vkfont@yandex.ru>
2017-03-19 16:11:17 +0300

    .

---
+++ src/index.js
@@ -0,0 +1,12 @@
+/**
+ * @api {get} /user/:id Request User information
+ * @apiName GetUser
+ * @apiGroup User
+ *
+ * @apiSchema (Body) {jsonschema=../schemas/test.req.json} apiParam
+ * @apiSchema (Body) {jsonschema=../schemas/test.req.json} apiParamExample
+ * @apiSchema {jsonschema=../schemas/test.res.json} apiSuccess
+ * @apiSchema {jsonschema=../schemas/test.res.json} apiSuccessExample
+ */
+
+// @apiErrorExample, @apiHeaderExample, @apiParamExample and @apiSuccessExample
